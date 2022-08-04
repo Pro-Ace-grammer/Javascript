c193b706faa3fc8f97437afa97d33569b346a0b0
Pro-Ace-grammer <arsalandarvajkar21@gmail.com>
2022-08-04 16:33:20 +0530

    added

---
+++ os-module.js
@@ -0,0 +1,8 @@
+//Built in Modules
+
+//os module
+const os = require(`os`)
+// Using os. we can use its many of the functionalities
+//example:- to get info about the current user
+const user = os.userInfo()   //Invoking module usin dot operator
+console.log(user);
