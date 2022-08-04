ba938bf31c9411b53b884a6fb3c631dae7510177
Pro-Ace-grammer <arsalandarvajkar21@gmail.com>
2022-08-04 16:18:56 +0530

    added and modified

---
+++ mind-granade.js
@@ -0,0 +1,10 @@
+//mind granade
+
+const n1=10
+const n2=32
+
+//using function keyword
+function add(){
+    console.log(`sum of two numbers is : ${ n1 + n2}`);
+}
+add()   //function is already invoked or called
--- sharemod2.js
+++ sharemod2.js
@@ -2,6 +2,11 @@
 
 const names = require('./sharemod1')    //require is for importing 
 const sayHi = require('./sharemod3')    
-sayHi('Arsalan')
-sayHi(names.alo)    //calling through object
-sayHi(names.blah)   //names is an object
+//sayHi('Arsalan')
+//sayHi(names.alo)    //calling through object
+//sayHi(names.blah)   //names is an object
+
+
+// To call the module(function) 
+require(`./mind-granade`)   //This gets executed automatically  
+                            //as it is already called
